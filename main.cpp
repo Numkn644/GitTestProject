8caf52ddc0f0f67e22c920baf21e3e2fa1368633
Numkn644 <numkn644@gmail.com>
2015-10-22 21:54:21 +0900

    main.cpp is changed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
+#include "Test.h"
 
 using namespace std;
 
 
 int main()
 {
+	Test test1("‚Ð‚ë‚ ‚«", 20);
+	Test test2("‰ª“c", 54);
+
 	cout << "This is Test." << endl;
+	test1.printStatus();
+	test2.printStatus();
+	
 
 	return 0;
 }
